1dc8a6057ac27b1004708b5f36527ec7f42538f1
Azule2 <69428406+Azule2@users.noreply.github.com>
2022-01-27 17:48:05 +0300

    Create Admin.py
    AdminPanel Code

---
+++ Admin.py
@@ -0,0 +1,10 @@
+Username = "admin"
+Password = "admin123"
+
+x = (input('enter Username ='))
+y = (input('enter Password ='))
+z = (input('enter Confirm Password ='))
+if  x == Username and y == Password and z == Password:
+    print('successful')
+else:
+    print('Filled')
